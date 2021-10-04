0bc0f1b01dec5304cf6d0abd9bfcda4245f3f98f
Mégas Vanzi <megas.vanzi@gmail.com>
2021-10-04 15:21:54 -0300

    add procedure

--- procedure.sql
+++ procedure.sql
@@ -6,3 +6,15 @@ $$
 language sql;
 
 select devolucion (37)	
+
+
+
+create function librosprestados () returns bigint
+as
+$$
+select count(*)
+from prestamos;
+$$
+language sql;		
+		
+select librosprestados();
